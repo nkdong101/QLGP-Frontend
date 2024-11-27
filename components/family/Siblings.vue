1014da31bbfe5f3ea4f387505a46133183b73141
duytrung93 <32829022+duytrung93@users.noreply.github.com>
2024-11-27 09:05:20 +0700

    ok

--- components/family/Siblings.vue
+++ components/family/Siblings.vue
@@ -199,7 +199,7 @@ export default {
                 method: "delete",
                 url: API.Giapha + "/" + obj.Id,
                 // params: Ơ,
-                action:  (re) => {
+                action: function (re) {
                   ShowMessage("Xóa thành công");
                   this.form.visible = false;
                   EventBus.$emit("reloadFormFam", this.obj.Curent.Id);
